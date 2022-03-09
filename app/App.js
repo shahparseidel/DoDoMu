e2c643731475ca67f4c558b35f3411d0c53a2ac2
Tanmay Dharmaraj <tanmay.dharmaraj@RapidCircle.com>
2022-03-09 13:24:20 +0100

    Initial setup for translations

--- app/App.js
+++ app/App.js
@@ -1,6 +1,7 @@
 import { SafeAreaView } from "react-native";
 import Main from "./screens/main";
 import { globalStyles } from "./styles/globalStyles";
+import i18n from "./locales/i18n"; //required to load translations
 
 export default function App() {
   return (
